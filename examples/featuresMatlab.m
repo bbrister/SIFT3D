c322f331001d8408819c94ea028547758fd5043f
Blaine Rister <blaine@stanford.edu>
2015-08-11 16:39:46 -0700

    removed redundant path initialization

--- examples/featuresMatlab.m
+++ examples/featuresMatlab.m
@@ -1,9 +1,3 @@
-% Change this to your toolbox path
-toolboxPath = '../build/wrappers/matlab';
-
-% Add the toolbox to the matlab path
-addpath(genpath(toolboxPath));
-
 % Load the data
 load data/data.mat
 
